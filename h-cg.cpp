22a4ebba15d8e1de634c08d1910bd6ac8fa64369
pdbruno <pdbruno@gmail.com>
2021-10-13 18:41:40 -0300

    pequeña correccion en CG

--- h-cg.cpp
+++ h-cg.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 // Heuristica constructiva golosa
 void H_CG(Grafo &G, int m, int n, vector<Vertice> &res) {
-	vector<Vertice> visitados(n, 0);
+	vector<bool> visitados(n, 0);
 	Vertice v = 0;
 	res[0] = v;
 
 	for (size_t i = 1; i < n; i++) {
 		Vertice w = get_min(G[v], visitados);
+		visitados[w] = true;
 		res[i] = w;
 		v = w;
 	}
 }
-Vertice get_min(vector<Vecino> &vecinos, vector<Vertice> &visitados) {
+Vertice get_min(vector<Vecino> &vecinos, vector<bool> &visitados) {
 
 	Vecino *minimo = nullptr;
 
 	for (size_t i = 0; i < vecinos.size(); i++) {
-		if (visitados[vecinos[i].dst] == 0 && (minimo == nullptr || minimo->peso > vecinos[i].peso)) {
+		if (!visitados[vecinos[i].dst] && (minimo == nullptr || minimo->peso > vecinos[i].peso)) {
 			minimo = &vecinos[i];
 		}
 	}
