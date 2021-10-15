4fc4f66f6c7bdc82169b67320fad41f114df3ffb
pdbruno <pdbruno@gmail.com>
2021-10-15 17:43:29 -0300

    termine el agm?

--- h-agm.cpp
+++ h-agm.cpp
@@ -1,31 +1,89 @@
 #include "types.cpp"
+#include <stack>
 using namespace std;
 
+#define MAX_INT 2147483647
 
 void H_AGM(Grafo &G, int m, int n, vector<Vertice> &res) {
-    //T = AGM(G)
-	vector<Vertice> T(n, -1);
-    AGM(G, T);
-    //E = duplicar aristas de T
-	vector<Vertice> E(n, -1);
-    //whatever(E)
-    //D = recorrer E usando DFS
-	vector<Vertice> D(n, -1);
-    DFS(E, D)
-    //H = armar un circuito hamiltoniano siguiendo el orden dado por D
 
-//chau me fui a comprar queso port salut light y entrenar
+    Grafo AGM(n, vector<Vecino>());
+    Prim(G, n, AGM);
 
-    //yo: de qué sirve duplicar las aristas en el segundo paso?
-    //profe: No es necesario hacerlo, es sólo para tratar de que sea más clara la demo de la cota. 
-    //Si tenés tiempo y ganas, mirá el video complementario que en el instante 2:20 menciono eso. Cualquier duda decime.
+    DFS(AGM, n, res);
+    res[n - 1] = 0; //pq el recorrido termina en el primer vertice, que es el 0
 }
 
-void AGM(Grafo &G, vector<Vertice> &res){
-    //hay que ver si usar Prim o Kruskal. 
-    //hoy pregunte en discord si vamos a trabajar solamente con grafos completos, que son super densos por lo que conviene usar Prim que es O(n²)
-    //en caso contrario, conviene usar algun algoritmo O(mlog(n))
+void Prim(Grafo &G, int n, Grafo &res) {
+
+    vector<bool> visitado(n, false);
+    vector<int> distancia(n, MAX_INT);
+    vector<Vertice> padre(n, -1);
+    Vertice s = 0;
+    for (Vecino &vecino : G[s]) {
+        distancia[vecino.dst] = vecino.peso;
+        padre[vecino.dst] = s;
+    }
+    distancia[s] = 0;
+    visitado[s] = true;
+
+    bool faltan_por_visitar = true;
+
+    while (faltan_por_visitar) {
+        faltan_por_visitar = false;
+        Vertice v = get_min(distancia, visitado); /*nodo de menor distancia para agregar que no fue visitado aun */
+        visitado[v] = true;
+        for (Vecino &vecino : G[v]) {
+            faltan_por_visitar = faltan_por_visitar || !visitado[vecino.dst];
+            distancia[vecino.dst] = vecino.peso;
+            padre[vecino.dst] = v;
+        }
+    }
+
+    for (size_t i = 1; i < n; i++) { //a partir del vector padre armo un AGM pero en formato grafo
+        Vertice actual = i;
+        Vertice Padre = padre[i];
+        Peso p = G[Padre][(actual < Padre) ? actual : actual - 1].peso;
+        res[padre[i]].push_back(Vecino(i, p));
+    }
+
+
 }
-void DFS(vector<Vertice> &E, vector<Vertice> &res){
-    //DFS bro
+
+void DFS(Grafo &T, int n, vector<Vertice> &res) {
+    int next = 0;
+    Vertice r = 0;
+    vector<bool> visitado(n, false);
+    stack<Vertice> lista;
+    lista.push(r);
+    visitado[r] = true;
+    res[next++] = r;
+    while (!lista.empty()) {
+        Vertice i = lista.top();
+        bool encontrado = false;
+        for (size_t j = 0; j < T[i].size() && !encontrado; i++) {
+            Vertice w = T[i][j].dst;
+            if (!visitado[w]) {
+                encontrado = true;
+                visitado[w] = true;
+                lista.push(w);
+                res[next++] = w;
+            }
+        }
+        if (!encontrado) {
+            lista.pop();
+        }
+    }
+
+}
+
+Vertice get_min(vector<int> &distancia, vector<bool> &visitados) {
+
+    Vertice minimo_no_visitado = -1;
+
+    for (size_t i = 0; i < visitados.size(); i++) {
+        if (!visitados[i] && (minimo_no_visitado == -1 || distancia[minimo_no_visitado] > distancia[i])) {
+            minimo_no_visitado = i;
+        }
+    }
+    return minimo_no_visitado;
 }
