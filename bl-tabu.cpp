b703578a30206e5819fdb8f93d84218173ec82de
Martin Saied <martin@datia.app>
2021-10-31 23:22:28 -0300

    Cosas

--- bl-tabu.cpp
+++ bl-tabu.cpp
@@ -5,39 +5,6 @@ using namespace std;
 
 #include <iterator>
 
-/* class VecindadStar
-{
-public:
-    struct Iterator 
-    {
-        using iterator_category = std::forward_iterator_tag;
-        using difference_type   = std::ptrdiff_t;
-        using value_type        = vector<Vertice>;
-        using pointer           = vector<Vertice>*;
-        using reference         = int&;
-
-        Iterator(pointer ptr) : m_ptr(ptr) {}
-
-        reference operator*() const { return *m_ptr; }
-        pointer operator->() { return m_ptr; }
-        Iterator& operator++() { m_ptr++; return *this; }  
-        Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
-        friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
-        friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };  
-
-    private:
-        pointer m_ptr;
-    };
-
-    VecindadStar()
-
-    Iterator begin() { return Iterator(&m_data[0]); }
-    Iterator end()   { return Iterator(&m_data[200]); }
-
-private:
-    int m_data[200];
-}; */
-
 // PARAMETROS QUE DEBERIAN LLEGAR A LA FUNCION
 int T;
 int MAX_ITERATIONS;
